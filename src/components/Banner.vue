90b4c68d3875eef22d960cb72e1cc0513c02f78d
Mauro Miguel <32025338+MauCoden95@users.noreply.github.com>
2023-01-07 02:05:29 -0300

    Segundo commit

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -2,6 +2,10 @@
     <section id="banner">
         <div class="center">
             <h1>Ser fuerte, estar en forma es nuestro lema</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et dolor id lectus placerat pulvinar. Fusce sit amet tincidunt erat. Integer eget ligula lobortis, scelerisque lectus id, sollicitudin quam.
+                Aliquam gravida libero nec neque placerat pretium. Duis a hendrerit turpis. Vestibulum lacus dui, volutpat sed euismod eu, bibendum ac arcu. 
+            </p>
             <a href="">Ver más</a>
         </div>
         
@@ -30,6 +34,12 @@ export default {
         flex-direction: column;
         justify-content: center;
         align-items: flex-start;
+    }
+
+    #banner p{
+        color: #fff;
+        font-size: 1.5rem;
+        text-align: justify;
     }
 
     #banner a{
